092cba9657a0fd530bd80e928048109bbde9135b
flowkater <chjw102@gmail.com>
2024-03-18 22:48:22 +0900

    apply secure storage

--- lib/page/main_page.dart
+++ lib/page/main_page.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:my_baseball_record/common/app_bar.dart';
 import 'package:my_baseball_record/common/bottom_navigation_bar.dart';
+import 'package:my_baseball_record/common/const/data.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  void initState() {
+    super.initState();
+
+    // checkToken();
+  }
+
+  void checkToken() async {
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+    print(accessToken);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const AppBarWidget(
-          trailingIcon: Icon(Icons.notifications_none),
+        title: AppBarWidget(
+          trailingIcon: GestureDetector(
+            onTap: () {
+              checkToken();
+            },
+            child: const Icon(
+              Icons.notifications_none,
+            ),
+          ),
         ),
       ),
       body: const SafeArea(
