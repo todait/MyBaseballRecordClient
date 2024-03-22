1047b5989dfe7cebbe86a16aa7f84940c6b5b008
sangwoo <kajer102@gmail.com>
2024-03-22 21:03:28 +0900

    BottomNavigation 작성 및 동작

--- lib/common/bottom_navigation_bar.dart
+++ lib/common/bottom_navigation_bar.dart
@@ -1,53 +1,86 @@
 import 'package:flutter/material.dart';
 import 'package:my_baseball_record/common/app_color.dart';
-import 'package:my_baseball_record/page/profile_page.dart';
-import 'package:my_baseball_record/page/record_page.dart';
+import 'package:my_baseball_record/common/app_text_list.dart';
+import 'package:my_baseball_record/common/app_text_style.dart';
 
-class BottomNavigationBarWidget extends StatefulWidget {
+class BottomNavigationBarWidget extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onTabIcon;
+
   const BottomNavigationBarWidget({
     super.key,
+    required this.selectedIndex,
+    required this.onTabIcon,
   });
 
   @override
-  State<BottomNavigationBarWidget> createState() =>
-      _BottomNavigationBarWidgetState();
-}
-
-class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
-  int selectedIndex = 0;
-
-  void onTabIcon(int index) {
-    setState(() {
-      selectedIndex = index;
-    });
-    navigatePage(index);
-  }
-
-  void navigatePage(int index) {
-    if (index == 0) {
-      null;
-    } else if (index == 1) {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const RecordPage()));
-    } else if (index == 2) {
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (context) => const ProfilePage()));
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    const Color selectedColor = AppColor.graysWhite;
+    const Color selectedColor = AppColor.textPrimary;
+    const Color unselectedColor = AppColor.textHint;
 
-    return BottomNavigationBar(
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.sports_baseball), label: '메인'),
-        BottomNavigationBarItem(icon: Icon(Icons.receipt), label: '기록'),
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle), label: '프로필'),
-      ],
-      selectedItemColor: selectedColor,
-      currentIndex: selectedIndex,
-      onTap: onTabIcon,
+    return Container(
+      height: 85,
+      decoration: BoxDecoration(
+        color: AppColor.graysWhite,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: AppColor.graysGray),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 24, right: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.sports_baseball),
+                  onPressed: () => onTabIcon(0),
+                  color: selectedIndex == 0 ? selectedColor : unselectedColor,
+                ),
+                Text(
+                  AppTextList.mainTitle,
+                  style: AppTextStyle.body413M.copyWith(
+                      height: 0.2,
+                      color:
+                          selectedIndex == 0 ? selectedColor : unselectedColor),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.sticky_note_2),
+                  onPressed: () => onTabIcon(1),
+                  color: selectedIndex == 1 ? selectedColor : unselectedColor,
+                ),
+                Text(
+                  AppTextList.recordTitle,
+                  style: AppTextStyle.body413M.copyWith(
+                      height: 0.2,
+                      color:
+                          selectedIndex == 1 ? selectedColor : unselectedColor),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.account_circle),
+                  onPressed: () => onTabIcon(2),
+                  color: selectedIndex == 2 ? selectedColor : unselectedColor,
+                ),
+                Text(
+                  AppTextList.profileTitle,
+                  style: AppTextStyle.body413M.copyWith(
+                      height: 0.2,
+                      color:
+                          selectedIndex == 2 ? selectedColor : unselectedColor),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
