803063092d1cd2ae9f0c8cf1b132ba58c46c3494
sangwoo <kajer102@gmail.com>
2024-04-03 00:48:09 +0900

    AppBottomSheet screenHeight 고려해서 size 수정 및 메서드 정의

--- lib/common/app_bottom_sheet.dart
+++ lib/common/app_bottom_sheet.dart
@@ -4,6 +4,12 @@ import 'package:my_baseball_record/common/app_color.dart';
 class AppBottomSheet {
   static void show(BuildContext context, Widget text1, Widget text2,
       Widget text3, Widget text4, Widget button1, Widget button2) {
+    final modalHeight = MediaQuery.of(context).size.height * 0.46;
+
+    double getHeight(double size) {
+      return modalHeight * (size / modalHeight);
+    }
+
     showModalBottomSheet(
       isScrollControlled: true,
       context: context,
@@ -15,21 +21,20 @@ class AppBottomSheet {
               24,
             ),
             child: SizedBox(
-              height: 388,
-              width: 393,
+              height: modalHeight,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const SizedBox(
-                    height: 24,
+                  SizedBox(
+                    height: getHeight(24),
                   ),
                   text1,
-                  const SizedBox(
-                    height: 16,
+                  SizedBox(
+                    height: getHeight(16),
                   ),
                   text2,
-                  const SizedBox(
-                    height: 40,
+                  SizedBox(
+                    height: getHeight(40),
                   ),
                   Row(
                     children: [
@@ -37,8 +42,8 @@ class AppBottomSheet {
                         Icons.check,
                         color: AppColor.primaryBlue2,
                       ),
-                      const SizedBox(
-                        width: 8,
+                      SizedBox(
+                        width: getHeight(8),
                       ),
                       text3,
                       const Spacer(),
@@ -48,12 +53,12 @@ class AppBottomSheet {
                       ),
                     ],
                   ),
-                  const SizedBox(
-                    height: 55,
+                  SizedBox(
+                    height: getHeight(55),
                   ),
                   button1,
-                  const SizedBox(
-                    height: 10,
+                  SizedBox(
+                    height: getHeight(10),
                   ),
                   button2,
                 ],
