c0f61c987b69397d2b37a9511994297dd04eae64
sangwoo <kajer102@gmail.com>
2024-04-22 14:14:27 +0900

    Gamecard에 있는 메서드를 분리해서 widget Class로 만들었습니다.

--- lib/common/game_card.dart
+++ lib/common/game_card.dart
@@ -3,6 +3,7 @@ import 'package:my_baseball_record/common/app_color.dart';
 import 'package:my_baseball_record/common/app_text_style.dart';
 import 'package:my_baseball_record/common/auth_button.dart';
 import 'package:my_baseball_record/common/match_status_widget.dart';
+import 'package:my_baseball_record/common/position_chip_widget.dart';
 
 class GameCard extends StatelessWidget {
   final int? totalNumber;
@@ -73,78 +74,6 @@ class GameCard extends StatelessWidget {
     }
   }
 
-  List<Widget> _getPositionChips(List<String> positions) {
-    return positions.map((position) {
-      if (position == "포지션 미정") {
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4),
-            border: Border.all(
-              color: AppColor.textHint,
-              width: 0.2,
-            ),
-          ),
-          child: Row(
-            children: [
-              const Icon(
-                Icons.emergency,
-                color: AppColor.accentRed100,
-                size: 10,
-              ),
-              const SizedBox(
-                width: 4,
-              ),
-              Text(
-                "포지션 미정",
-                style: AppTextStyle.body413M.copyWith(
-                  color: AppColor.textHint,
-                ),
-              ),
-            ],
-          ),
-        );
-      } else {
-        Color chipColor;
-        String chipText;
-        TextStyle textStyle;
-
-        switch (position) {
-          case "타자":
-            chipColor = AppColor.additionalPurple10;
-            chipText = "타자";
-            textStyle = AppTextStyle.caption113B1
-                .copyWith(color: AppColor.additionalPurple100);
-            break;
-          case "투수":
-            chipColor = AppColor.additionalOrange10;
-            chipText = "투수";
-            textStyle = AppTextStyle.caption113B1
-                .copyWith(color: AppColor.additionalOrange100);
-            break;
-          default:
-            chipColor = AppColor.textHint;
-            chipText = position;
-            textStyle =
-                AppTextStyle.caption113B1.copyWith(color: AppColor.textHint);
-        }
-
-        return Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-          margin: const EdgeInsets.only(right: 4),
-          decoration: BoxDecoration(
-            color: chipColor,
-            borderRadius: BorderRadius.circular(4),
-          ),
-          child: Text(
-            chipText,
-            style: textStyle,
-          ),
-        );
-      }
-    }).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     final matchStatus = _getMatchStatus();
@@ -193,9 +122,7 @@ class GameCard extends StatelessWidget {
                   ),
                 ],
               ),
-              Wrap(
-                children: _getPositionChips(positions),
-              )
+              PositionChipWidget(positions: positions),
             ],
           ),
           const SizedBox(height: 26),
